e3326cbb64bcf53614733421f28f27672240ff42
Mustofa <mustofa_sachi@hotmail.com>
2023-03-25 09:30:54 +0700

    add quiz result

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import Layout from './components/Layout'
 import './styles/Quiz.scss'
 import QuizCard from './components/QuizCard'
@@ -9,23 +9,43 @@ import { ScoreContext } from './App'
 const Quiz = () => {
   const { score } = useContext(ScoreContext)
 
+  const [isDone, setIsDone] = useState(false)
+
   return (
     <Layout>
-      <div className='quiz'>
-        <h1>แบบทดสอบก่อนเรียน (Pretest)</h1>
+      {isDone ? (
+        <div className='quiz'>
+          <div className='quiz__result'>
+            <h3>You obtained</h3>
+            <h1>
+              7<span>/15</span>
+            </h1>
+            <h3>on this quiz/test</h3>
 
-        <div className='quiz__title-box'>
-          <h3>จงเลือกคำตอบที่ถูกเพียง 1 ข้อ</h3>
+            <h3>
+              <i>The submission was made on 24-Mar-2023 @ 10:30:53</i>
+            </h3>
+          </div>
         </div>
+      ) : (
+        <div className='quiz'>
+          <h1>แบบทดสอบก่อนเรียน (Pretest)</h1>
 
-        {questions.map((element, index) => (
-          <QuizCard key={index} id={index + 1} question={element} />
-        ))}
+          <div className='quiz__title-box'>
+            <h3>จงเลือกคำตอบที่ถูกเพียง 1 ข้อ</h3>
+          </div>
 
-        <Button type='primary' onClick={() => console.log(score)}>
-          ส่งคำตอบ
-        </Button>
-      </div>
+          {questions.map((element, index) => (
+            <QuizCard key={index} id={index + 1} question={element} />
+          ))}
+
+          <div>
+            <Button type='primary' onClick={() => console.log(score)}>
+              ส่งคำตอบ
+            </Button>
+          </div>
+        </div>
+      )}
     </Layout>
   )
 }
